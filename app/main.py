9e929c2e9201741e016d55e6d3367a68d6d57533
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-02-24 09:22:06 +0200

    fixed mutable type

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ class Cargo:
 class BaseRobot:
     def __init__(self, name: str,
                  weight: float,
-                 coords: list = None) -> None:
-        if coords is None:
+                 coords: list = tuple) -> None:
+        if coords is tuple:
             coords = [0, 0]
         self.weight = weight
         self.name = name
@@ -34,8 +34,8 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        if coords is None:
+    def __init__(self, name: str, weight: int, coords: list = tuple) -> None:
+        if coords is tuple:
             coords = [0, 0, 0]
         super().__init__(name, weight, coords)
 
@@ -52,9 +52,9 @@ class DeliveryDrone(FlyingRobot):
     def __init__(self, name: str,
                  weight: int,
                  max_load_weight: int | float,
-                 coords: list = None,
+                 coords: list = tuple,
                  current_load: Cargo | None = None) -> None:
-        if coords is None:
+        if coords is tuple:
             coords = [0, 0, 0]
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
